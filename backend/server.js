44190680ac46fd4053f67bebaaf7344cc76fa042
ramyajalluri7 <ramyajalluri2007@gmail.com>
2026-09-22 15:27:25 +0530

    Completed project

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
+
 dotenv.config();
 
 const app = express();
 
-// Connect Database
 connectDB();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
-// Test Route
+
 app.get("/", (req, res) => {
   res.send("ExamX Backend Running...");
 });
